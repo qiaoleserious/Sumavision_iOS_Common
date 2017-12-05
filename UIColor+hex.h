6bab665c789c69b6708cb7d7f4e9b320b40167f0
山谷里没有枪声 <1007793974@qq.com>
2017-12-05 16:18:29 +0800

    Add files via upload

---
+++ UIColor+hex.h
@@ -0,0 +1,18 @@
+//
+//  UIColor+hex.h
+//  ipad开发
+//
+//  Created by 乔乐 on 2017/11/8.
+//  Copyright © 2017年 乔乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#define RGBA_COLOR(R, G, B, A) [UIColor colorWithRed:((R) / 255.0f) green:((G) / 255.0f) blue:((B) / 255.0f) alpha:A]
+#define RGB_COLOR(R, G, B) [UIColor colorWithRed:((R) / 255.0f) green:((G) / 255.0f) blue:((B) / 255.0f) alpha:1.0f]
+@interface UIColor (hex)
++ (UIColor *)colorWithHexString:(NSString *)color;
+
+//从十六进制字符串获取颜色，
+//color:支持@“#123456”、 @“0X123456”、 @“123456”三种格式
++ (UIColor *)colorWithHexString:(NSString *)color alpha:(CGFloat)alpha;
+@end
